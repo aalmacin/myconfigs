12a94e7e552f144b342e91c9713d45d1f7b2c984
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-04-28 14:12:11 -0400

    Aliased checkout and status

--- config_all.sh
+++ config_all.sh
@@ -2,6 +2,8 @@ cp -i .vimrc ~
 git config --global core.excludesfile ~/.gitignore
 git config --global user.name "Aldrin Jerome Almacin"
 git config --global user.email "julienaldrin@gmail.com"
+git config --global alias.co checkout
+git config --global alias.stat status
 cp -i .gitignore ~
 
 cp -i nerdtree.zip ~/.vim/nerdtree.zip
